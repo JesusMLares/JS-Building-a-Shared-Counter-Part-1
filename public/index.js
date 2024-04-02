7129e8619d60ea31fe92a72e81259494adc59b01
JesusMLares <twinlares2@hotmail.com>
2024-04-01 20:01:25 -0700

    fetch counter = 10

--- public/index.js
+++ public/index.js
@@ -1,9 +1,17 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    
+
+    let response = await fetch('http://localhost:9001/counter')
+    
+    let responseText = await response.json()
+
+    console.log(responseText)
+    
+    let countValue = responseText.value;
 
     function increment(){
         countValue++;
